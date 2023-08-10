705c9a5801ad4f9897d9eeb1a20c50714bc3c1cf
jarry <acbbc70412@gmail.com>
2023-08-10 13:48:37 +0800

    init git

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import TransiotionEffect from '@/components/TransiotionEffect'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
